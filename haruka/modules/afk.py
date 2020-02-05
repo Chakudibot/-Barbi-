b2a8c5e2b241992ad3308382948bf8794399eecf
Chakudibot <60508203+Chakudibot@users.noreply.github.com>
2020-02-06 00:11:17 +0530

    Update afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -27,7 +27,7 @@ def afk(bot: Bot, update: Update):
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
-    update.effective_message.reply_text(tld(chat.id, f"{fname} is now AFK!"))
+    update.effective_message.reply_text(tld(chat.id, f"{fname} એ હવે વાત નહિ કરે))
 
 
 @run_async
@@ -42,7 +42,7 @@ def no_longer_afk(bot: Bot, update: Update):
     if res:
         firstname = update.effective_user.first_name
         try:
-            update.effective_message.reply_text(tld(chat.id, f"{firstname} is no longer AFK!"))
+            update.effective_message.reply_text(tld(chat.id, f"{firstname} એ હવે વાત કરશે"))
         except:
             return
 
