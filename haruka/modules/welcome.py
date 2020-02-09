d590d233d9d15c02f09e0ae17667e19d4491f55c
Chakudibot <60508203+Chakudibot@users.noreply.github.com>
2020-02-09 21:47:08 +0530

    Update welcome.py

--- haruka/modules/welcome.py
+++ haruka/modules/welcome.py
@@ -113,8 +113,7 @@ def new_member(bot: Bot, update: Update):
                     "I have been added to {} with ID: <pre>{}</pre>".format(chat.title, chat.id),
                     parse_mode=ParseMode.HTML
                 )
-                bot.send_message(chat.id, "Thanks for adding me into your group! Don't forgot to checkout our news channel!")
-
+                bot.send_message(chat.id, "મને તમારા ગ્રુપ મા આમંત્રણ આપવા બદલ આપનો આભાર! અમારી ચેનલ ને જોવાનું ચૂકશો નહિ!")
             else:
                 # If welcome message is media, send with appropriate function
                 if welc_type != sql.Types.TEXT and welc_type != sql.Types.BUTTON_TEXT:
